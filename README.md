c75d7372167ac1cfc97f94326232e08dcc4e7510
Xin Deng <xind1470@gmail.com>
2023-10-25 14:09:45 -0700

    Renamed origin, trying to see if push works

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Lab 2
+# Lab 2, 3
 
 ## About Me Pt.1
 
@@ -13,7 +13,7 @@ Build out an “About Me” webpage that quizzes the user on fun facts gives the
 
 ### Lighthouse Accessibility Report Score
 
-![Lighthouse report](img/lighthouse.png)
+![Lighthouse report for Lab 2](img/lighthouse2.png)
 
 ### Reflections and Comments
 
@@ -25,3 +25,28 @@ Build out an “About Me” webpage that quizzes the user on fun facts gives the
 
 3. How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
     - Almost 7 hours. That is too long. I had to idea how long it would take me, but I know that is too long.
+
+
+## About Me Pt.2
+
+Additional functionality added to your About Me Guessing Game. A "Top Ten" favorites list is added. Sixth question added so user can guess a number with only four tries given. Seventh question added with multiple answers in an array with six tries given to the user. 
+
+### Author: Xin Deng
+
+### Links and Resources
+
+1. [Setting Up a New Code 201 Project](https://codefellows.github.io/code-201-guide/curriculum/class-02/project-setup)
+
+
+### Lighthouse Accessibility Report Score
+
+![Lighthouse report for Lab 2](img/lighthouse2.png)
+
+### Reflections and Comments
+
+1. How did this go, overall?
+  
+2. What observations or questions do you have about what you’ve learned so far?
+
+3. How long did it take you to complete this assignment? And, before you started, how long did you think it would take you to complete this assignment?
+
--- js/app.js
+++ js/app.js
@@ -1,17 +1,14 @@
 "use strict";
 
 //Ask user's name
-
 let userName = prompt("What is your name?");
 console.log(userName);
-
 //console.log(Hey, ${userName},! Welcome to my site! Let's answer some questions to get to know me.);
 alert(
   `Hey, ${userName}! Welcome to my site! Let's answer some questions to get to know me.`
 );
 
 // Question 1
-
 let cat = prompt("Do you think I have 3 cat? Yes or No?");
 cat = cat.toLowerCase();
 console.log(cat);
@@ -95,5 +92,66 @@ switch (travel) {
     alert("Very wrong! I've been in 10 countries total!");
     break;
 }
+
+//Question 6 too high too low loops 4 attempts
+//When a user enters information through the prompt() function in JavaScript, the input is always treated as a string, even if the user enters a number. The Number() function is often used to convert that string representation of a number into an actual numeric value.
+
+let age = 25
+let attempts = 4
+
+for( let i = 1; i <= attempts; i++ ) {
+   let ageGuess = prompt("Guess how old I am?");
+   guess = Number(ageGuess);
+
+   if( guess === age ) {
+    alert("Congrats! You guessed correctly! I am 25!");
+    break;
+   } else if (guess < age) {
+    alert (" Too low! I wish I was younger... Try Again!");
+   } else {
+    alert ("Too high! I'm not that old! Try Again! ");
+   }
+}
+if (i === attempts)
+
+
+
+
+
+
+
+
+
 // Final message
 alert(`Thanks for getting to know me, ${userName}!`);
+
+
+
+
+
+/*
+str="John" str.charat(0), str.substring (0,1)
+let dogs = ["Rosie", "Geno", "Rocky"];
+
+let gotItRight = false;
+
+while( ! gotItRight ) {
+  let guess = prompt("What is the name of a dog of mine?");
+  if( dogs.includes( guess ) ) {
+    alert("Right!");
+    gotItRight = true;
+  }
+}
+
+// Let them try 4
+for( let i = 1; i < 5; i++ ) {
+
+   let guess = prompt("How old am I?");
+   // guess = "55"
+   guess = Number(guess);
+   if( guess === 55 ) {
+    alert("Right!");
+    break;
+   }
+
+}
